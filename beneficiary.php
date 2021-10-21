11a8e1cecb8f8f7b87e42a0aa3f2bfce25b03920
ElectronAAB <bandhanabdullahalamgir@gmail.com>
2021-10-21 14:00:43 +0600

    first commit

---
+++ beneficiary.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+</head>
+
+<h1> Page 2 [Add Beneficiary] </h1>
+
+<p><b> Digital Wallet </b></p>
+
+<p> 1. <a href="home.php">Home</a> 2.  <a href="beneficiary.php">Add Beneficiary</a> 3. <a href="transaction.php">Transaction History</a><p>
+
+<b>Add Beneficiary:</b>
+
+Beneficiary Name: <input type="text" name="beneficiary"> Mobile No: <input type="text" name="mobile"> <input type="submit" value="SUBMIT">
+<body>
+<table>
+<tr>
+  <th>Mir Md. kawsur</th>
+  <th>01515012203</th>
+</tr>
+<tr>
+  <td>Rifat</td>
+  <td>01919772244</td>
+  </tr>
+</table>
+</body>
